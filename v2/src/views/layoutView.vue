e4cfcdc21b8ac117648bbded4d9baf35226cfa88
maple <maple@mapledeMacBook.local>
2018-11-22 04:21:19 +0800

    提交

--- v2/src/views/layoutView.vue
+++ v2/src/views/layoutView.vue
@@ -1,11 +1,17 @@
 <template>
-  <transition name="page-move">
-    <router-view class="cube-view"></router-view>
+  <transition :name="active">
+    <router-view class="pages-view"></router-view>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  export default {
+    data() {
+      return {
+        active: 'page-fade'
+      }
+    }
+  }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -13,4 +19,9 @@
   transform translate(100%, 0)
 .page-move-enter-active, .page-move-leave-active
   transition transform 0.3s
+
+.page-fade-enter, .page-fade-leave-active
+  opacity 0
+.page-fade-enter-active, .page-fade-leave-active
+  transition opacity .2s
 </style>
